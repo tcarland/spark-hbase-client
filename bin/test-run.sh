1d28e3311a36c7d605a61a49f8a9131b8f197356
Timothy C. Arland <tcarland@gmail.com>
2017-07-25 21:18:43 -0700

    build test jar and versioning

--- bin/test-run.sh
+++ bin/test-run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #
 
-
 export HADOOP_CLASSPATH="$HBASE_HOME/conf:$HBASE_HOME/lib/*"
+
+
+TARGET_JAR="target/spark-hbase-client-test-1.0.3.jar"
+TARGET_CLASS="com.trace3.hbase.HBaseTest"
+
 
 spark-submit --master yarn \
 --num-executors 4 \
---class com.trace3.bdi.HBaseTest \
-target/hbase-client-test-1.0.2.jar $@
-
+--class $TARGET_CLASS \
+$TARGET_JAR $@
 
 
---
+++ src/test/resources/build-test-jar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#  Build a test jar
+#  Run 'mvn package test' first
+#
+
+TARGET_JAR="target/spark-hbase-client-test-1.0.3.jar"
+
+
+jar -cvf $TARGET_JAR -C target/classes . -C target/test-classes .
+

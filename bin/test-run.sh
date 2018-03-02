1aad5db59b3d90f805da67c0de3cac8ee91870e0
tca <tcarland@gmail.com>
2018-03-02 06:21:26 -0800

    fix test class

--- bin/test-run.sh
+++ bin/test-run.sh
@@ -5,10 +5,10 @@ export HADOOP_CLASSPATH="$HBASE_HOME/conf:$HBASE_HOME/lib/*"
 
 
 TARGET_JAR="target/spark-hbase-client-test-1.1.0.jar"
-TARGET_CLASS="com.trace3.hbase.HBaseTest"
+TARGET_CLASS="HBaseTest"
 
 
 spark-submit --master yarn \
---num-executors 4 \
---class $TARGET_CLASS \
-$TARGET_JAR $@
+ --num-executors 4 \
+ --class $TARGET_CLASS \
+ $TARGET_JAR $@

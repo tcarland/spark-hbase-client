45fdae45588475262848903f0fe159bd31a054fb
tca <tcarland@gmail.com>
2018-03-07 09:56:29 -0800

    set version properly

--- src/test/resources/build-test-jar.sh
+++ src/test/resources/build-test-jar.sh
@@ -3,7 +3,7 @@
 #  Run 'mvn package test' first
 #
 
-TARGET_JAR="target/spark-hbase-client-test-1.1.1.jar"
+TARGET_JAR="target/spark-hbase-client-test-1.1.3.jar"
 
 
 jar -cvf $TARGET_JAR -C target/classes . -C target/test-classes .

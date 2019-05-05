b49d31d4e6482d7af23340c91fc539ce51e8f15b
Timothy C. Arland <tcarland@users.noreply.github.com>
2019-05-05 08:04:37 -0700

    sync updates to scala / spark version (#6)

--- src/test/resources/build-test-jar.sh
+++ src/test/resources/build-test-jar.sh
@@ -3,7 +3,8 @@
 #  Run 'mvn package test' first
 #
 
-TARGET_JAR="target/spark-hbase-client-test-1.1.5.jar"
+TARGET_JAR="target/spark-hbase-client-test-1.1.6.jar"
 
+( mvn scala:testCompile )
 
-jar -cvf $TARGET_JAR -C target/classes . -C target/test-classes .
+( jar -cvf $TARGET_JAR -C target/classes . -C target/test-classes . )

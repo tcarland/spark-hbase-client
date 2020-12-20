103c3d485926205936124d8352acbfc39370cb39
Timothy C. Arland <tcarland@users.noreply.github.com>
2020-12-20 10:21:32 -0800

    V1.2.1 (#9)
    * update hbase version to match current TDH
    * updates for hadoop&spark v3

--- src/test/resources/build-test-jar.sh
+++ src/test/resources/build-test-jar.sh
@@ -3,6 +3,6 @@
 #  Run 'mvn package test' first
 #
 
-TARGET_JAR="target/spark-hbase-client-test-1.2.0.jar"
+TARGET_JAR="target/spark-hbase-client-test-1.3.1.jar"
 
 ( jar -cvf $TARGET_JAR -C target/classes . -C target/test-classes . )

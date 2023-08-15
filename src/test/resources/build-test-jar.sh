3d0959dab2e5bf1463765d5f33f3748b702a52c0
Timothy C. Arland <tcarland@gmail.com>
2023-08-15 11:18:11 -0600

    update to java 17

--- src/test/resources/build-test-jar.sh
+++ src/test/resources/build-test-jar.sh
@@ -2,6 +2,6 @@
 #  Build a test jar
 #  Run 'mvn package test' first
 #
-TARGET_JAR="target/spark-hbase-client-test-1.5.2.jar"
+TARGET_JAR="target/spark-hbase-client-test-1.5.3.jar"
 
 ( jar -cvf $TARGET_JAR -C target/classes . -C target/test-classes . )
